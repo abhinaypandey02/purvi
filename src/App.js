16dba53f9dd43f52a0501aed774efd708c49a784
Purvi Chaurasia <chaurasiapurvi@gmail.com>
2024-01-10 11:20:12 +0530

    Modified dropdown options

--- src/App.js
+++ src/App.js
@@ -7,53 +7,83 @@ const value_inputs = [
     title:'Logical Flow and Decision Node Accuracy',
     options:[
       {
-        label:'Test',
-        value:'test'
+        label:'1',
+        value:'1'
       },
       {
-        label:'Test2',
-        value:'test2'
+        label:'2',
+        value:'2'
+      },
+      {
+        label:'3',
+        value:'3'
       },
     ]
   },
   {
-    title:'Value 2',
+    title:'Complexity and Length',
     options:[
       {
-        label:'Test',
-        value:'test'
+        label:'1',
+        value:'1'
       },
       {
-        label:'Test2',
-        value:'test2'
+        label:'2',
+        value:'2'
+      },
+      {
+        label:'3',
+        value:'3'
       },
     ]
   },
   {
-    title:'Value 3',
+    title:'Alignment with Process Goals',
 
     options:[
       {
-        label:'Test',
-        value:'test'
+        label:'1',
+        value:'1'
       },
       {
-        label:'Test2',
-        value:'test2'
+        label:'2',
+        value:'2'
+      },
+      {
+        label:'3',
+        value:'3'
       },
     ]
   },
   {
-    title:'Value 4',
+    title:'Multiple Start Nodes or Subgraphs',
 
     options:[
       {
-        label:'Test',
-        value:'test'
+        label:'0',
+        value:'0'
       },
       {
-        label:'Test2',
-        value:'test2'
+        label:'1',
+        value:'1'
+      },
+    ]
+  },
+  {
+    title:'Overall Quality Score',
+
+    options:[
+      {
+        label:'1',
+        value:'1'
+      },
+      {
+        label:'2',
+        value:'2'
+      },
+      {
+        label:'3',
+        value:'3'
       },
     ]
   },
